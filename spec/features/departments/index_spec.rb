466ab625902dd7a9eff5a772637605ed315c722d
Nick Sacco <nicksacco10@gmail.com>
2023-04-09 21:28:11 -0700

    added seeds

--- spec/features/departments/index_spec.rb
+++ spec/features/departments/index_spec.rb
@@ -11,6 +11,20 @@ RSpec.describe 'Department index page' do
     @ted = @sales.employees.create!(name: "Ted Tedston", level: 6)
     @miller = @software.employees.create!(name: "Miller Mills", level: 3)
     @phil = @software.employees.create!(name: "Phil Walter", level: 1)
+
+    @ticket_1 = Ticket.create!(subject: "New Computer", age: 4)
+    @ticket_2 = Ticket.create!(subject: "Mini fridge", age: 1)
+    @ticket_3 = Ticket.create!(subject: "Lunch Money", age: 2)
+    @ticket_4 = Ticket.create!(subject: "Office Supplies", age: 3)
+    @ticket_5 = Ticket.create!(subject: "Broken Printer", age: 9)
+
+
+    @bill_computer = EmployeeTicket.create!(employee: @bill, ticket: @ticket_1)
+    @will_fridge = EmployeeTicket.create!(employee: @will, ticket: @ticket_2)
+    @bill_lunch = EmployeeTicket.create!(employee: @bill, ticket: @ticket_3)
+    @ted_printer = EmployeeTicket.create!(employee: @ted, ticket: @ticket_5)
+    @miller_printer = EmployeeTicket.create!(employee: @miller, ticket: @ticket_5)
+    @phil_supplies = EmployeeTicket.create!(employee: @phil, ticket: @ticket_4)
   end
 
   it "shows each department's name and floor" do
